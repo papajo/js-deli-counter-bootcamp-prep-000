0e105e8b387d9bed6be5aabda58934968807a41a
PJ Joshi <joshipv2@gmail.com>
2019-03-28 03:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,15 @@ function nowServing(currLine) {
 }
 
 function currentLine(currLine) {
+  const tempCurr = []
   if (currLine.length === 0) {
     return "The line is currently empty."
   } else {
       for (let i = 0; i < currLine.length; i++) {
-          return `The Line is currently: ${i+1}. ${currLine[i]}, `
+        tempCurr[i] = currLine[i]  
       }
+      
   }
+  return `The Line is currently: ${i+1}. ${tempCurr[i]}, `
 }
 
